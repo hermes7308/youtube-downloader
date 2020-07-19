d7fac47f548ed3042ba704baec74a88361bf5981
hermes7308 <hermes7308@gmail.com>
2020-07-19 07:50:21 -0400

    package 구조 변경 docker 적용중

--- flask/config.py
+++ flask/config.py
@@ -12,12 +12,10 @@ class ReleaseConfig(Config):
 
 
 class DevelopConfig(Config):
-    LOGGING_LEVEL = logging.INFO
-
-    HOME_PATH = "/home/irteam/data/youtube"
+    pass
 
 
 class LocalConfig(Config):
     LOGGING_LEVEL = logging.DEBUG
 
-    HOME_PATH = "C:\\Users\\herme\\Desktop\\video\\youtube"
+    HOME_PATH = "C:\\youtubedownloader\\data\\youtube"
